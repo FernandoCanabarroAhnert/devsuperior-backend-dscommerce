d794772df2b6e9649c70029d90884a5c590035be
FernandoCanabarroAhnert <ahnertfernando499@gmail.com>
2024-06-07 21:19:22 -0300

    :sparkles: feat: Endpoint para buscar categorias

---
+++ src/main/java/com/devsuperior/dscommerce/services/CategoryService.java
@@ -0,0 +1,22 @@
+package com.devsuperior.dscommerce.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.dscommerce.dtos.CategoryDTO;
+import com.devsuperior.dscommerce.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    private CategoryRepository repository;
+
+    @Transactional(readOnly = true)
+    public List<CategoryDTO> findAll(){
+        return repository.findAll().stream().map(CategoryDTO::new).toList();
+    }
+}
